9494e8ee8291346ae64c285d4646b5d0fd6ddf3a
Keelen Fisher <keelencarrera@gmail.com>
2022-07-26 16:37:31 -0500

    added chartjs and canvas

--- js/app.js
+++ js/app.js
@@ -131,16 +131,108 @@ function handleClick(event){
 
 function handleShowResults(){
   if(totalVotes === 0){
-    for(let i = 0; i < allProducts.length; i++){
-      let liElem = document.createElement('li');
-      liElem.textContent = `${allProducts[i].name} had ${allProducts[i].votes}, and was seen ${allProducts[i].views} times.`;
-      resultsList.appendChild(liElem);
-    }
+    renderChart();
+    // for(let i = 0; i < allProducts.length; i++){
+    //   let liElem = document.createElement('li');
+    //   liElem.textContent = `${allProducts[i].name} had ${allProducts[i].votes}, and was seen ${allProducts[i].views} times.`;
+    //   resultsList.appendChild(liElem);
+    // }
+
     resultBtn.removeEventListener('click', handleShowResults);
   }
 }
 
+// *********** CHART DISPLAY ***************
 
+// *** CANVAS ELEMENT TO RENDER THE CHART ***
+
+let canvasElem = document.getElementById('my-chart');
+
+function renderChart(){
+
+  // *** CREATING EMPTY ARRAYS TO POPULATE WITH THE INFO ****
+
+  let productNames = [];
+  let productVotes = [];
+  let productViews = [];
+
+  //  ** Creating a loop that takes all the data after the voting rounds are completed and populates the arrays **
+
+  for(let i = 0; i < allProducts.length; i++){
+    productNames.push(allProducts[i].name);
+    productViews.push(allProducts[i].views);
+    productVotes.push(allProducts[i].votes);
+  }
+  let myObj = {
+    type: 'bar',
+    data: {
+      labels: productNames,
+      datasets: [{
+        label: '# of Votes',
+        data: productVotes,
+        backgroundColor: [
+          '#ff7300',
+          '#fffb00',
+          '#48ff00',
+          '#00ffd5',
+          '#002bff',
+          '#7a00ff',
+          '#ff00c8',
+          '#ff0000'
+        ],
+        borderColor: [
+          '#ff7300',
+          '#fffb00',
+          '#48ff00',
+          '#00ffd5',
+          '#002bff',
+          '#7a00ff',
+          '#ff00c8',
+          '#ff0000'
+        ],
+        borderWidth: 1
+      },
+      {
+        label: '# of Views',
+        data: productViews,
+        backgroundColor: [
+          '#ff0000',
+          '#ff7300',
+          '#fffb00',
+          '#48ff00',
+          '#00ffd5',
+          '#002bff',
+          '#7a00ff',
+          '#ff00c8',
+          '#ff0000'
+        ],
+        borderColor: [
+          '#ff0000',
+          '#ff7300',
+          '#fffb00',
+          '#48ff00',
+          '#00ffd5',
+          '#002bff',
+          '#7a00ff',
+          '#ff00c8',
+          '#ff0000'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  };
+
+  // *** CONSTRUCTOR CALL TO RENDER THE CHART ***
+  new Chart(canvasElem, myObj);
+
+}
 
 
 // **************** Event listeners **************
